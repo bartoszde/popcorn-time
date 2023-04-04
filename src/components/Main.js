a88e89694aede6e3c51f61f87d4ba7e19d23ef93
BD <bartoszdubinski@iMac.local>
2023-04-04 14:36:53 +0200

    add footer, header and main

---
+++ src/components/Main.js
@@ -0,0 +1,24 @@
+import movies from "../data/movies.json";
+
+import "./Main.css"
+
+function Main(){
+
+    return(
+        <div className="Main">
+            {movies.map( movieObj => {
+                return(
+                    <div key={movieObj.id} className="card">
+                        <h2>{movieObj.title}</h2>
+                        <h3>Rating: {movieObj.rating}</h3>
+                        <h3>Year: {movieObj.year}</h3>
+
+                        <button>Delete</button>
+                    </div>
+                );
+            })}
+        </div>
+    )
+}
+
+export default Main;
